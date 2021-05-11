346c79f0a0667b80302e5e07004ea2b4ab22bf14
kennedyfitzz <82603835+kennedyfitzz@users.noreply.github.com>
2021-05-11 09:34:59 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,31 @@
 scene.setBackgroundColor(6)
+let Steave = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c . . . 
+    . . . . . . . c c d d d d c . . 
+    . . . . . c c c c c c d d c . . 
+    . . . c c c 4 4 4 4 d c c c c c 
+    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+    . . f 4 4 1 4 c c 4 4 d f . . . 
+    . . . f d 4 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Player)
+for (let index = 0; index < 1; index++) {
+    Steave.setPosition(145, 85)
+    pause(200)
+    Steave.setPosition(135, 85)
+    pause(200)
+    Steave.setPosition(125, 85)
+    pause(200)
+    Steave.setPosition(116, 85)
+}
 let Bob = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . 5 5 5 . . . . 
@@ -19,25 +46,6 @@ let Bob = sprites.create(img`
     `, SpriteKind.Player)
 Bob.setPosition(24, 86)
 Bob.say("HI")
-let Steave = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . c c c c . . . 
-    . . . . . . . c c d d d d c . . 
-    . . . . . c c c c c c d d c . . 
-    . . . c c c 4 4 4 4 d c c c c c 
-    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
-    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
-    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
-    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
-    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
-    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
-    . . f 4 4 1 4 c c 4 4 d f . . . 
-    . . . f d 4 4 4 4 4 4 c f . . . 
-    . . . . f f 4 4 4 4 c d b c . . 
-    . . . . . . f f f f d d d c . . 
-    . . . . . . . . . . c c c . . . 
-    `, SpriteKind.Player)
-Steave.setPosition(129, 86)
 pause(2000)
 Steave.say("Hello!")
 pause(2000)
